319f41dab4ee8712873b575fd7c16e22cc256a09
Hojae Jung <ghwo8844@gmail.com>
2017-02-04 22:18:22 -0600

    Switched API

---
+++ src/GooglePlacesAPI.java
@@ -0,0 +1,61 @@
+/**
+ * Created by Hojae Jung on 2/3/2017.
+ */
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+class GooglePlacesAPI {
+    private double lat, lon;
+    private CloseableHttpClient client;
+    private String APIKey;
+
+    public GooglePlacesAPI() throws IOException {
+        client = HttpClients.createDefault();
+        BufferedReader getAPIKey = new BufferedReader(new FileReader(new File("GooglePlacesAPIKey.txt")));
+        APIKey = getAPIKey.readLine().trim();
+        getAPIKey.close();
+        lat = 0;
+        lon = 0;
+    }
+
+    public GooglePlacesAPI(double lat, double lon) throws IOException {
+        this();
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public JsonObject getSearchResult(String query) {
+        try {
+            URI uri = new URIBuilder("https://maps.googleapis.com/maps/api/place/textsearch/json")
+                    .addParameter("query", query)
+                    .addParameter("latitude", Double.toString(lat))
+                    .addParameter("longitude", Double.toString(lon))
+                    .addParameter("radius", "30000")
+                    .addParameter("key", APIKey)
+                    .build();
+            HttpGet httpGet = new HttpGet(uri);
+            HttpResponse response = client.execute(httpGet);
+            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            JsonParser parser = new JsonParser();
+            JsonObject jo = parser.parse(br.readLine()).getAsJsonObject();
+            br.close();
+            return jo;
+        } catch(URISyntaxException e) {
+            System.err.println("Google API disabled");
+        } catch(IOException e) {
+            System.err.println("No Response Error");
+        }
+        return null;
+    }
+}
